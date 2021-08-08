ee06fb14ed08433966531693b25e0cbcf94386a2
wuny <wuny511@gmail.com>
2021-08-08 23:31:38 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Selenium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.keyword_textbox = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(369, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "검색";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 95);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(614, 423);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "No.";
+            this.columnHeader1.Width = 35;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "제목";
+            this.columnHeader2.Width = 392;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "날짜";
+            this.columnHeader3.Width = 105;
+            // 
+            // keyword_textbox
+            // 
+            this.keyword_textbox.Location = new System.Drawing.Point(12, 25);
+            this.keyword_textbox.Name = "keyword_textbox";
+            this.keyword_textbox.Size = new System.Drawing.Size(139, 21);
+            this.keyword_textbox.TabIndex = 2;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(177, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 16);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "관련도순";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(271, 30);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 16);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "최신순";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10",
+            "20",
+            "30"});
+            this.comboBox1.Location = new System.Drawing.Point(249, 60);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(81, 20);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 530);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.keyword_textbox);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TextBox keyword_textbox;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Selenium
+{
+    public partial class Form1 : Form
+    {
+        const string _apiUrl = IgnoreValues.apiUrl;
+        const string _ClientId = IgnoreValues.ClientId;
+        const string _ClientSecret = IgnoreValues.ClientSecret;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            data_Parsing();
+        }
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            string results = getResult();
+            string text = listView1.FocusedItem.SubItems[0].Text;
+            var i = Convert.ToInt32(text);
+            var parseJson = JObject.Parse(results);
+            var link = parseJson["items"][(i - 1)]["link"].ToString();
+            Process.Start("chrome.exe", link);
+        }
+        
+        private string getResult()
+        {
+
+            string keyword = keyword_textbox.Text;
+            
+            string display = comboBox1.Text;
+            string sort = "sim";
+            if (radioButton2.Checked == true)
+            {
+                sort = "date";
+            }
+
+            string query = string.Format("?query={0}&display={1}&sort={2}", keyword, display, sort);
+
+            WebRequest request = WebRequest.Create(_apiUrl + query);
+            request.Headers.Add("X-Naver-Client-Id", _ClientId);
+            request.Headers.Add("X-Naver-Client-Secret", _ClientSecret);
+
+            string requestResult = "";
+            using(var response = request.GetResponse())
+            {
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    using(var reader = new StreamReader(dataStream))
+                    {
+                        requestResult = reader.ReadToEnd();
+                    }
+                }
+            }
+            return requestResult;
+        }
+        private void data_Parsing()
+        {
+            try
+            {
+                string results = getResult();
+                results = results.Replace("<b>", "");
+                results = results.Replace("</b>", "");
+
+                var parseJson = JObject.Parse(results);
+                var countsOfDisplay = Convert.ToInt32(parseJson["display"]);
+                var countsOfResults = Convert.ToInt32(parseJson["total"]);
+                listView1.Items.Clear();
+                for (int i = 0; i < countsOfDisplay; i++)
+                {
+                    ListViewItem item = new ListViewItem((i + 1).ToString());
+                    var title = parseJson["items"][i]["title"].ToString();
+                    title = title.Replace("&quot;", "\"");
+
+                    var date = parseJson["items"][i]["pubDate"].ToString();
+                    date = date.Replace("Jan", "1")
+                                 .Replace("Feb", "2")
+                                 .Replace("Mar", "3")
+                                 .Replace("Apr", "4")
+                                 .Replace("May", "5")
+                                 .Replace("Jun", "6")
+                                 .Replace("Jul", "7")
+                                 .Replace("Aug", "8")
+                                 .Replace("Sep", "9")
+                                 .Replace("Oct", "10")
+                                 .Replace("Nov", "11")
+                                 .Replace("Dec", "12");
+                    string[] split_date;
+                    split_date = date.Split(' ');
+
+                    date = split_date[3] + "." + split_date[2] + "." + split_date[1];
+
+                    item.SubItems.Add(title);
+                    item.SubItems.Add(date);
+                    listView1.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+
+    }
+}
